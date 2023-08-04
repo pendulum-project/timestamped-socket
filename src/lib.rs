13da6e4db1243fa65bfc5511f649d08e8051a299
Folkert <folkert@folkertdev.nl>
2023-08-04 16:16:53 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+mod control_message;
+pub mod interface;
+pub mod raw_udp_socket;
+pub mod timestamped_udp_socket;
+
+/// Turn a C failure (-1 is returned) into a rust Result
+pub(crate) fn cerr(t: libc::c_int) -> std::io::Result<libc::c_int> {
+    match t {
+        -1 => Err(std::io::Error::last_os_error()),
+        _ => Ok(t),
+    }
+}
