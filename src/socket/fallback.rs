88ba03345199f95108158179d69c128f3c61f7da
David Venhoek <david@tweedegolf.com>
2024-03-01 14:37:21 +0100

    Fixed configure_timestamping interface on other OSes.

--- src/socket/fallback.rs
+++ src/socket/fallback.rs
@@ -5,6 +5,7 @@ use super::InterfaceTimestampMode;
 pub(super) fn configure_timestamping(
     _socket: &RawSocket,
     mode: InterfaceTimestampMode,
+    _bind_phc: Option<u32>,
 ) -> std::io::Result<()> {
     match mode {
         InterfaceTimestampMode::None => Ok(()),
