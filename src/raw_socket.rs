055f480a74dce0559262456768b2f3a3900455a4
David Venhoek <david@tweedegolf.com>
2026-02-25 15:23:21 +0100

    Implemented destination addresses for ipv6.

--- src/raw_socket.rs
+++ src/raw_socket.rs
@@ -45,6 +45,25 @@ impl RawSocket {
         })
     }
 
+    pub(crate) fn enable_destination_ipv6(&self) -> std::io::Result<()> {
+        // SAFETY:
+        //
+        // - the socket is provided by (safe) rust, and will outlive the call
+        // - method is guaranteed to be a valid "name" argument
+        // - the options pointer outlives the call
+        // - the `option_len` corresponds with the options pointer
+        unsafe {
+            cerr(libc::setsockopt(
+                self.fd,
+                libc::IPPROTO_IPV6,
+                libc::IPV6_RECVPKTINFO,
+                &(1 as libc::c_int) as *const _ as *const libc::c_void,
+                std::mem::size_of::<libc::c_int>() as libc::socklen_t,
+            ))?;
+        }
+        Ok(())
+    }
+
     pub(crate) fn bind(&self, addr: sockaddr_storage) -> std::io::Result<()> {
         // Per posix, it may be invalid to specify a length larger than that of the family.
         let len = sockaddr_len(addr);
