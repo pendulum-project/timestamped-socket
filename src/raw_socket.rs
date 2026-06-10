7aed3f71178db696e69e664b439a46a407583155
David Venhoek <david@tweedegolf.com>
2026-06-10 15:20:38 +0200

    Add support for ipv4 and ipv6 only sockets not bound to a specific interface.

--- src/raw_socket.rs
+++ src/raw_socket.rs
@@ -49,6 +49,25 @@ impl RawSocket {
         })
     }
 
+    pub(crate) fn ipv6_only(&self) -> std::io::Result<()> {
+        // SAFETY:
+        //
+        // - the socket is provided by (safe) rust, and will outlive the call
+        // - method is guaranteed to be a valid "name" argument
+        // - the options pointer outlives the call
+        // - the `option_len` corresponds with the options pointer
+        unsafe {
+            cerr(libc::setsockopt(
+                self.fd,
+                libc::IPPROTO_IPV6,
+                libc::IPV6_V6ONLY,
+                &(1 as libc::c_int) as *const _ as *const libc::c_void,
+                std::mem::size_of::<libc::c_int>() as libc::socklen_t,
+            ))?;
+        }
+        Ok(())
+    }
+
     pub(crate) fn enable_destination_ipv6(&self) -> std::io::Result<()> {
         // SAFETY:
         //
